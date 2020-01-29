0b39c4df9db7019e3411d0bb59372af3521b16f0
rossrco <rossrco@gmail.com>
2020-01-29 13:27:08 +0100

    add data ingestion to the trainer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,11 +1,22 @@
 from __future__ import print_function
+from io import BytesIO
+
+from google.cloud import storage
+
+import pandas as pd
 
 import argparse
 
 
 def train(**args):
-    print('Training...')
-    pass
+    print('Extracting data.')
+    client = storage.Client()
+    bucket = client.get_bucket('ames-house-dataset')
+    blob = storage.Blob('train.csv', bucket)
+    content = blob.download_as_string()
+    data = pd.read_csv(BytesIO(content), index_col=0)
+    print('Data extracted.')
+    print(data.head())
 
 
 if __name__ == '__main__':
